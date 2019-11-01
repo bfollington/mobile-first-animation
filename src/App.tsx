f7d43264c1cfe73f9af9ac1e1a8524e1713b7e3c
Ben Follington <5009316+bfollington@users.noreply.github.com>
2019-11-01 14:53:13 +1000

    Demo page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { DragContainer } from './DragContainer'
+import { HorizontalScrollList } from './HorizontalScrollList'
+
+const items = [
+  {
+    title: 'First item',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQujZCExt_1x9mXOn9oddCbXRpFQB2RLtoz2sCBaRdIeRHgRV4y',
+  },
+  {
+    title: 'Second item',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQujZCExt_1x9mXOn9oddCbXRpFQB2RLtoz2sCBaRdIeRHgRV4y',
+  },
+  {
+    title: 'Third item',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQujZCExt_1x9mXOn9oddCbXRpFQB2RLtoz2sCBaRdIeRHgRV4y',
+  },
+  {
+    title: 'Fourth item',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQujZCExt_1x9mXOn9oddCbXRpFQB2RLtoz2sCBaRdIeRHgRV4y',
+  },
+  {
+    title: 'Fifth item',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQujZCExt_1x9mXOn9oddCbXRpFQB2RLtoz2sCBaRdIeRHgRV4y',
+  },
+  {
+    title: 'Sixth item',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQujZCExt_1x9mXOn9oddCbXRpFQB2RLtoz2sCBaRdIeRHgRV4y',
+  },
+]
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DragContainer />
+
+      <HorizontalScrollList items={items} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
